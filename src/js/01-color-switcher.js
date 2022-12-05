1f9c3bda31d19fe4661ec684b4f57651f2c83bfa
Ihor Melnykov <imelya17@gmail.com>
2022-12-05 19:41:59 +0200

    1st done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,38 @@
+const body = document.querySelector('body');
+const startButton = document.querySelector('button[data-start]');
+const stopButton = document.querySelector('button[data-stop]');
 
+
+let timerId = null;
+
+stopButton.disabled = true;
+
+function disableBtn(btn) {
+  btn.disabled = true;
+}
+function enableBtn(btn) {
+  btn.disabled = false;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function changeBgColor() {
+  body.style.backgroundColor = getRandomHexColor();
+}
+
+function onStartButtonClick() {
+  timerId = setInterval(changeBgColor, 1000);
+  disableBtn(startButton);
+  enableBtn(stopButton);
+}
+
+function onStopButtonClick() {
+  clearInterval(timerId);
+  disableBtn(stopButton);
+  enableBtn(startButton);
+}
+
+startButton.addEventListener('click', onStartButtonClick);
+stopButton.addEventListener('click', onStopButtonClick);
